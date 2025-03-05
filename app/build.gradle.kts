8e0abdc6477e99c3464564e0485b3a41e5ba52b7
warunagit <waruna.git@gmail.com>
2025-03-05 17:05:37 +0200

    comments added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
--- app/src/main/java/com/cycolabs/p29coroutines/MainActivity.kt
+++ app/src/main/java/com/cycolabs/p29coroutines/MainActivity.kt
@@ -7,6 +7,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //android use main thread to execute all executions like
+    //drawing the views, executing logical code pices in a sequel manner..
+    //So, when creating an app, should not block the main thread.
+    //Should use multi-threaded approch to perform nework operations and logical operations
+    //Multithreading starts with Async task->RxJava->Coroutines
+    //Multithreading works in prallel ways
+    //Coroutines are light-weight threads, run in parellel, can wait for each other, performance optimized
+    //allows asynchronus, synchronus programming
+    //allows execution to be suspend and resume
+    //3 Levels: Global Scope - starts and ends with the app
+    //Lifecycle Scope - stops when activity destroy, but should check activity status and cancel.
+    //ViewModel Scope - same like lifecycle scope, but will live as viewmodel alive.
+    //
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
