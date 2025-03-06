a22b4c4d025d6ec472399625445bf4d40334aba1
warunagit <waruna.git@gmail.com>
2025-03-06 17:08:02 +0200

    coroutine switch start

--- app/src/main/java/com/cycolabs/p29coroutines/MainActivity.kt
+++ app/src/main/java/com/cycolabs/p29coroutines/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.cycolabs.p29coroutines
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.cycolabs.p29coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     //android use main thread to execute all executions like
@@ -23,17 +26,53 @@ class MainActivity : AppCompatActivity() {
     //.Main - UI non blocking operations. UI related events android main/ui thread
     //.IO - network and disk operations. netorking or read/write files or any input/output
     //.DEFAULT - if not specified, this will use. sort large list, complex calculations
-    //Couroutines start using .LAUNCH builder. Starts the corouting and return it as a job
+    //Couroutines start using .LAUNCH builder. Starts the corouting and returns it as a job
+    //**only the original thread that created view, can touch its view.
+    //inside coroutines, to switch between Dispatchers, should use withContext() method
+    //CoroutineScope(Dispatchers.Main).launch -> withContext(Dispatchers.Main)
 
+    private lateinit var binding: ActivityMainBinding
+    private var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //without coroutine, it crashes
+        binding.btnCount.setOnClickListener{
+            binding.tvTitle.text = counter++.toString()
+        }
+
+        binding.btnDownload.setOnClickListener {
+            downloadBigFile()
+        }
+
+        //with coroutine, it continues
+        sayHelloFromMainThred()
+        sayHelloFromBackgroundThread()
+
+        //switch between coroutines
+        binding.btnSwitchCoroutine.setOnClickListener {
+            binding.btnSwitchCoroutine.text =
         }
     }
+
+    private fun downloadBigFile() {
+        for (i in 0..1000000){
+            Log.i("TAGY", "Downloading in thread $1 in ${Thread.currentThread().name}")
+        }
+    }
+
+    private fun sayHelloFromMainThred() {
+        CoroutineScope(Dispatchers.Main).launch {
+            binding.text1.text = "Hello from ${Thread.currentThread().name}"
+        }
+    }
+    private fun sayHelloFromBackgroundThread() {
+        CoroutineScope(Dispatchers.IO).launch {
+            binding.text2.text = "Hello from ${Thread.currentThread().name}"
+        }
+    }
+
+
 }
